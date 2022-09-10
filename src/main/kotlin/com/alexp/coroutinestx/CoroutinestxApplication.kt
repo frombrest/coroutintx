be1875664af3dd024ddb4e33846d963263601a9c
Aliaksandr Parfianiuk <aparfianiuk@atlassian.com>
2022-09-10 13:09:17 -0700

    Initial commmit

---
+++ src/main/kotlin/com/alexp/coroutinestx/CoroutinestxApplication.kt
@@ -0,0 +1,11 @@
+package com.alexp.coroutinestx
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoroutinestxApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoroutinestxApplication>(*args)
+}
---
+++ src/test/kotlin/com/alexp/coroutinestx/CoroutinestxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alexp.coroutinestx
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoroutinestxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
